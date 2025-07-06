2a4e8ea16bf02306c700721cbe18dfba84585069
alicia1989123 <aliciabasilo.ab@gmail.com>
2025-07-06 14:24:57 -0600

    Added loggin

--- security_group_updater.py
+++ security_group_updater.py
@@ -2,6 +2,11 @@ import boto3
 import requests
 import json
 import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 KEY = os.getenv("AWS_ACCESS_KEY_ID")
 SECRET = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -26,8 +31,13 @@ def get_current_ip():
 def read_last_ip():
     try:
         response = ssm.get_parameter(Name="/infra/ip/last", WithDecryption=False)
+        logger.info(f"SSM response: {response}")
         return response["Parameter"]["Value"]
     except ssm.exceptions.ParameterNotFound:
+        logger.warning("Parameter not found, returning None")
+        return None
+    except Exception as e:
+        logger.error(f"Error reading IP from SSM: {e}")
         return None
 
 def save_current_ip(ip):
@@ -38,9 +48,9 @@ def save_current_ip(ip):
             Type="String",
             Overwrite=True
         )
-        print(f"✔ Saved IP: {ip}")
+        logger.info(f"✔ Saved IP: {ip}")
     except Exception as e:
-        print(f"❌ Failed to save IP to SSM: {e}")
+        logger.info(f"❌ Failed to save IP to SSM: {e}")
 
 def trigger_lambda(ip):
     payload = {"ip": ip}
@@ -49,18 +59,20 @@ def trigger_lambda(ip):
         InvocationType="Event",
         Payload=json.dumps(payload)
     )
-    print("✅ Lambda triggered, status:", response["StatusCode"])
+    logger.info(f"✅ Lambda triggered, status: {response["StatusCode"]}")
 
 def main():
     current_ip = get_current_ip()
     last_ip = read_last_ip()
 
     if current_ip != last_ip:
-        print(f"🔄 IP changed: {last_ip} → {current_ip}")
+        logger.info(f"🔄 IP changed: {last_ip} → {current_ip}")
         trigger_lambda(current_ip)
         save_current_ip(current_ip)
     else:
-        print("✅ IP unchanged.")
+        logger.info("✅ IP unchanged.")
 
 if __name__ == "__main__":
+    
+    logger.info("Starting security group updater")
     main()
